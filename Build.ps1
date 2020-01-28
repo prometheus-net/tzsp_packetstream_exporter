5979116c4db6ff1159b6ae9c7d00b4d8a67f6ada
Sander Saares <sander@saares.eu>
2020-01-28 09:55:50 +0200

    Package files in build script

--- Build.ps1
+++ Build.ps1
@@ -9,3 +9,12 @@ dotnet publish -c Release -r win-x64
 if ($LASTEXITCODE -ne 0) {
   Write-Error "Build failed for win-64"
 }
+
+if (!(Test-Path ./Output/)) {
+  New-Item -ItemType Directory ./Output/ | Out-Null
+}
+
+Get-ChildItem ./Output/ | Remove-Item
+
+Compress-Archive -Path ./TzspPacketStreamExporter/bin/Release/netcoreapp3.1/linux-x64/publish/* -DestinationPath ./Output/linux-x64.zip
+Compress-Archive -Path ./TzspPacketStreamExporter/bin/Release/netcoreapp3.1/win-x64/publish/* -DestinationPath ./Output/win-x64.zip
