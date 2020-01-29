5a6c6c1d227f8c0e86f630ad15713c572dbdf328
Sander Saares <sander@saares.eu>
2020-01-29 10:13:43 +0200

    How about now

--- Build.ps1
+++ Build.ps1
@@ -1,25 +1,32 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "Current directory is: "
-Get-Location | Out-Host
+Push-Location
+try {
+  Set-Location $PSScriptRoot
 
-Get-ChildItem
+  Write-Host "Current directory is: "
+  Get-Location | Out-Host
 
-dotnet publish -c Release -r linux-x64
-if ($LASTEXITCODE -ne 0) {
-  Write-Error "Build failed for linux-64"
+  Get-ChildItem
+
+  dotnet publish -c Release -r linux-x64
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Build failed for linux-64"
+  }
+
+  dotnet publish -c Release -r win-x64
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Build failed for win-64"
+  }
+
+  if (!(Test-Path ./Output/)) {
+    New-Item -ItemType Directory ./Output/ | Out-Null
+  }
+
+  Get-ChildItem ./Output/ | Remove-Item
+
+  Compress-Archive -Path ./bin/Release/netcoreapp3.1/linux-x64/publish/* -DestinationPath ./Output/linux-x64.zip
+  Compress-Archive -Path ./bin/Release/netcoreapp3.1/win-x64/publish/* -DestinationPath ./Output/win-x64.zip
+} finally {
+  Pop-Location
 }
-
-dotnet publish -c Release -r win-x64
-if ($LASTEXITCODE -ne 0) {
-  Write-Error "Build failed for win-64"
-}
-
-if (!(Test-Path ./Output/)) {
-  New-Item -ItemType Directory ./Output/ | Out-Null
-}
-
-Get-ChildItem ./Output/ | Remove-Item
-
-Compress-Archive -Path ./bin/Release/netcoreapp3.1/linux-x64/publish/* -DestinationPath ./Output/linux-x64.zip
-Compress-Archive -Path ./bin/Release/netcoreapp3.1/win-x64/publish/* -DestinationPath ./Output/win-x64.zip
